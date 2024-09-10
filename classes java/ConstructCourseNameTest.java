f6029641838c55588feccec22857376cc7dbce1c
pedrovicto <victorpedrosilva445@gmail.com>
2024-09-10 12:54:11 -0300

    create class Account

--- classes java/ConstructCourseNameTest.java
+++ classes java/ConstructCourseNameTest.java
@@ -1,7 +1,7 @@
 public class ConstructCourseNameTest {
     public static void main(String[] args) {
-        ConstructCourseName constructCourseName1 = new ConstructCourseName( "Introduction to java programming" );
-        ConstructCourseName constructCourseName2 = new ConstructCourseName( "Html, css, javaScript" );
+        ConstructCourseName constructCourseName1 = new ConstructCourseName( "Introduction to java programming\n" );
+        ConstructCourseName constructCourseName2 = new ConstructCourseName( "Html, css, javaScript\n" );
 
         System.out.printf( "gradebook course name is: \n%s", constructCourseName1.getConstructCourseName() );
         System.out.printf( "gradebook course name is: \n%s", constructCourseName2.getConstructCourseName() );
